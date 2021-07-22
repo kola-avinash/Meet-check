0619418c7b59c62c1a12bad7c2b3cdf6c6c87032
Avinash Kola <avinashkola2001@gmail.com>
2021-07-22 17:18:04 +0530

    navbar completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $(".navbar-toggler").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+});
+
+
 // nav bar scroll background change
 
 $(document).ready(function(){
